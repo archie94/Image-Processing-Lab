18932a560d57ea3784fc6ce082e1d5eaec78bb09
archie94 <arkaprava94@gmail.com>
2016-11-19 20:15:14 +0530

    Robert

---
+++ Class/Day 2/robert.py
@@ -0,0 +1,21 @@
+# Sharpening filter operations - Robert
+
+import cv2
+import math
+import copy
+import numpy as np 
+from matplotlib import pyplot as plt 
+
+img = cv2.imread('../../Image/bike.jpg', 0)
+
+kernel_x = np.array( [ [1,0], [0,-1] ] )
+kernel_y = np.array( [ [0,1], [-1,0] ] )
+
+out_img = cv2.filter2D(img, -1, kernel_x)
+out_img2 = cv2.filter2D(out_img, -1, kernel_y)
+
+plt.subplot(121), plt.imshow(img, cmap='gray'), plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(out_img2, cmap='gray'), plt.title('Sharpened with Robert')
+plt.xticks([]), plt.yticks([])
+plt.show()
