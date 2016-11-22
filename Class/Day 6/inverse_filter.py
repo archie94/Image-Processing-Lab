5340dccde17163a4cf10443bfbd713bb2b93a7c2
archie94 <arkaprava94@gmail.com>
2016-11-22 11:43:32 +0530

    Inverse filter

---
+++ Class/Day 6/inverse_filter.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt 
+
+img = cv2.imread('../../Image/low_contrast.jpg', 0)
+
+img2 = img
+
+img2 = 255 - img2
+
+plt.subplot(121), plt.imshow(img, cmap='gray'), plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(img2, cmap='gray'), plt.title('Negative transformation')
+plt.xticks([]), plt.yticks([])
+plt.show()
